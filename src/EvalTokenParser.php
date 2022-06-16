832a6c55d91ca2fd0c0c4f5ebf4ff4561597e03d
Denis Brumann <denis.brumann@qossmic.com>
2022-06-16 13:29:11 +0200

    Drop support for Twig 1 and add Twig 3 support.

--- src/EvalTokenParser.php
+++ src/EvalTokenParser.php
@@ -4,22 +4,23 @@ declare(strict_types=1);
 
 namespace jæm3l\Twig;
 
+use Twig\Token;
 use Twig\TokenParser\AbstractTokenParser;
 
 class EvalTokenParser extends AbstractTokenParser
 {
-    public function parse(\Twig_Token $token)
+    public function parse(Token $token)
     {
         $stream = $this->parser->getStream();
 
-        $stream->expect(\Twig_Token::BLOCK_END_TYPE);
+        $stream->expect(Token::BLOCK_END_TYPE);
         $content = $this->parser->subparse([$this, 'decideEvalEnd'], true);
-        $stream->expect(\Twig_Token::BLOCK_END_TYPE);
+        $stream->expect(Token::BLOCK_END_TYPE);
 
         return new EvalNode([$content], [], $token->getLine(), $this->getTag());
     }
 
-    public function decideEvalEnd(\Twig_Token $token): bool
+    public function decideEvalEnd(Token $token): bool
     {
         return $token->test('endeval');
     }
